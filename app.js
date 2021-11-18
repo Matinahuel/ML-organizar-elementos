50ac13e28b02ced39060a2d720208b8b81f6c725
Matinahuel <matiasnahuel2396@gmail.com>
2021-11-17 21:38:03 -0300

    tp terminado

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+
+app.use(express.static('public'));
+
+app.listen(port, () => console.log(`servidor alojado en http://localhost:${port}`));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname,'views', '/home.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname,'views', '/register.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname,'views', '/login.html')));
